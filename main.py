c0edef23cbd74245fcfd970dcbdd1ebeac0ae245
gcsaba99 <gergely.csaba99@gmail.com>
2023-07-15 11:49:25 +0200

    new model, support for 1080p

--- main.py
+++ main.py
@@ -38,11 +38,34 @@ def is_tc_queue(image):
         return False
 
 
+crops_1440p = [(0, 0, 50, 50), (50, 0, 100, 50), (100, 0, 150, 50)]
+crops_1080p = [(0, 0, 36, 36), (36, 0, 72, 36), (72, 0, 108, 36)]
+
+region_1440p = (0, 77, 200, 50)
+region_1080p = (0, 58, 112, 36)
+
+region = region_1080p
+crops = crops_1080p
+
+full_screen_shot = pyautogui.screenshot()
+screenshot_size = full_screen_shot.size
+
+
+if screenshot_size == (1920, 1080):
+    region = region_1080p
+    crops = crops_1080p
+elif screenshot_size == (2560, 1440):
+    region = region_1440p
+    crops = crops_1440p
+else:
+    print('⚠️ This resolution is not configured, the program will probably not work.')
+
+print(f"ℹ️ Screen size is {screenshot_size}px")
+
 # 3 = (0, 77, 200, 50)
 # 1 = (0, 77, 50, 50)
-region = (0, 77, 200, 50)
 
-img_counter = 115
+img_counter = 500
 
 
 pressed_keys = set()
@@ -73,7 +96,7 @@ def on_press(key):
     if all(k in pressed_keys for k in combination_on_off):
         manual_on_off = not manual_on_off
         icon = '🟢' if manual_on_off else '🔴'
-        print(f'{icon} Toggle play_voice: ' + str(manual_on_off))
+        print(f'{icon} Toggle play voice: ' + str(manual_on_off))
     if all(k in pressed_keys for k in combination_add_tc):
         number_of_town_centers += 1
         if number_of_town_centers > 3:
@@ -85,9 +108,9 @@ def on_press(key):
             number_of_town_centers = 1
         print('ℹ️ number of Town Centers: ' + str(number_of_town_centers))
     if key == keyboard.Key.f11:
-        # file_path = os.path.join('negatives', f'{img_counter}.png')
+        # file_path = os.path.join('positives', f'{img_counter}.png')
         # img_counter += 1
-        # screenshot = pyautogui.screenshot(region=(50, 77, 50, 50))
+        # screenshot = pyautogui.screenshot(region=(0, 58, 36, 36))  # region=(50, 77, 50, 50)
         # screenshot.save(file_path)
         pass
 
@@ -130,7 +153,7 @@ def main_loop():
         screenshot = pyautogui.screenshot(region=region)
         play_voice = False
 
-        slots = [screenshot.crop((0, 0, 50, 50)), screenshot.crop((50, 0, 100, 50)), screenshot.crop((100, 0, 150, 50))]
+        slots = [screenshot.crop(crops[0]), screenshot.crop(crops[0]), screenshot.crop(crops[0])]
 
         for i, slot in enumerate(slots):
             if is_tc_queue(slot):
